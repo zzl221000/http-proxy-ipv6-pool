adb2e36837b6e56d364a42919c81311b9c63039e
wmfy808 <wmfysaddas@qq.com>
2024-08-17 23:46:39 +0800

    修复Bug

--- src/proxy.rs
+++ src/proxy.rs
@@ -156,7 +156,6 @@ impl Proxy {
         interface: String,
         gateway: String,
     ) -> Result<Response<Body>, hyper::Error> {
-        // 提取远程地址
         let remote_addr = match req.uri().authority().map(|auth| auth.to_string()) {
             Some(addr) => addr,
             None => {
@@ -167,7 +166,6 @@ impl Proxy {
             }
         };
 
-        // 手动提取 OnUpgrade
         let client_upgrade = match req.extensions_mut().remove::<OnUpgrade>() {
             Some(upgrade) => upgrade,
             None => {
@@ -178,71 +176,31 @@ impl Proxy {
             }
         };
 
-        // 异步处理隧道连接
-        let status = Arc::new(Mutex::new(None));
-
-        let status_clone = Arc::clone(&status);
-        tokio::spawn(async move {
-            let mut client = match client_upgrade.await {
-                Ok(upgraded) => upgraded,
-                Err(e) => {
-                    println!("Failed to upgrade client connection: {:?}", e);
-                    *status_clone.lock().await = Some(false);
-                    return;
-                }
-            };
-
-            let result = if let Err(e) = self.tunnel(&mut client, remote_addr, is_system_route, interface, gateway).await {
-                println!("Tunnel error: {:?}", e);
-                false
-            } else {
-                true
-            };
-
-            *status_clone.lock().await = Some(result);
-        });
-
-        let final_status = *status.lock().await;
-        match final_status {
-            Some(true) => Ok(Response::new(Body::empty())),
-            Some(false) => Ok(Response::builder()
-                .status(StatusCode::SERVICE_UNAVAILABLE)
-                .body(Body::from("Service Unavailable"))
-                .unwrap()),
-            None => Ok(Response::new(Body::empty())),
-        }
-    }
-
-    async fn tunnel<A>(
-        &self,
-        client: &mut A,
-        addr_str: String,
-        is_system_route: bool,
-        interface: String,
-        gateway: String,
-    ) -> Result<(), io::Error>
-    where
-        A: AsyncRead + AsyncWrite + Unpin + ?Sized,
-    {
         let timeout_duration = DEFAULT_TIMEOUT;
 
-        let addrs = match addr_str.to_socket_addrs() {
+        let addrs = match remote_addr.to_socket_addrs() {
             Ok(addrs) => addrs.collect::<Vec<_>>(),
             Err(e) => {
-                println!("Invalid address: {}: {:?}", addr_str, e);
-                return Err(io::Error::new(io::ErrorKind::InvalidInput, "Invalid address"));
+                println!("Invalid address: {}: {:?}", remote_addr, e);
+                return Ok(Response::builder()
+                    .status(StatusCode::SERVICE_UNAVAILABLE)
+                    .body(Body::from("Service Unavailable"))
+                    .unwrap());
             }
         };
 
         if addrs.is_empty() {
-            return Err(io::Error::new(io::ErrorKind::NotFound, "No valid addresses resolved"));
+            println!("No valid addresses resolved");
+            return Ok(Response::builder()
+                .status(StatusCode::SERVICE_UNAVAILABLE)
+                .body(Body::from("Service Unavailable"))
+                .unwrap());
         }
 
-        let addr = addrs[0]; // 使用解析到的第一个地址
-
+        let addr = addrs[0];
         let socket = match addr {
-            SocketAddr::V4(_) => TcpSocket::new_v4()?,
-            SocketAddr::V6(_) => TcpSocket::new_v6()?,
+            SocketAddr::V4(_) => TcpSocket::new_v4().unwrap(),
+            SocketAddr::V6(_) => TcpSocket::new_v6().unwrap(),
         };
 
         let bind_addr = match addr {
@@ -251,7 +209,16 @@ impl Proxy {
         };
 
         if is_system_route {
-            let cmd_str = format!("ip addr add {}/{} dev {}", bind_addr.ip(), if bind_addr.is_ipv6() { self.ipv6_prefix_len } else { self.ipv4_prefix_len }, interface);
+            let cmd_str = format!(
+                "ip addr add {}/{} dev {}",
+                bind_addr.ip(),
+                if bind_addr.is_ipv6() {
+                    self.ipv6_prefix_len
+                } else {
+                    self.ipv4_prefix_len
+                },
+                interface
+            );
             self.execute_command(cmd_str).await;
 
             if !gateway.is_empty() {
@@ -267,41 +234,52 @@ impl Proxy {
             self.manage_address_count(&interface).await;
         }
 
-        // 绑定到指定的本地 IP 地址
-        socket.bind(bind_addr)?;
+        if socket.bind(bind_addr).is_err() {
+            println!("Failed to bind to address");
+            return Ok(Response::builder()
+                .status(StatusCode::SERVICE_UNAVAILABLE)
+                .body(Body::from("Service Unavailable"))
+                .unwrap());
+        }
 
-        // 使用 timeout 包装连接操作，以防止长时间阻塞
         let connect_result = timeout(timeout_duration, socket.connect(addr)).await;
-
         let mut server = match connect_result {
             Ok(Ok(stream)) => stream,
             Ok(Err(e)) => {
                 println!("Failed to connect to server: {:?}", e);
-                return Err(io::Error::new(io::ErrorKind::ConnectionRefused, "Failed to connect"));
+                return Ok(Response::builder()
+                    .status(StatusCode::SERVICE_UNAVAILABLE)
+                    .body(Body::from("Service Unavailable"))
+                    .unwrap());
             }
             Err(_) => {
-                println!("Connection to {} timed out", addr_str);
-                return Err(io::Error::new(io::ErrorKind::TimedOut, "Connection timed out"));
+                println!("Connection to {} timed out", remote_addr);
+                return Ok(Response::builder()
+                    .status(StatusCode::SERVICE_UNAVAILABLE)
+                    .body(Body::from("Service Unavailable"))
+                    .unwrap());
             }
         };
+        tokio::spawn(async move {
+            match timeout(timeout_duration, tokio::io::copy_bidirectional(&mut client_upgrade.await.unwrap(), &mut server)).await {
+                Ok(Ok((client_bytes, server_bytes))) => {
+                    println!("Client wrote {} bytes, server wrote {} bytes", client_bytes, server_bytes);
 
-        // 使用 timeout 包装双向数据传输，以防止长时间阻塞
-        match timeout(timeout_duration, tokio::io::copy_bidirectional(client, &mut server)).await {
-            Ok(Ok((client_bytes, server_bytes))) => {
-                println!("Client wrote {} bytes, server wrote {} bytes", client_bytes, server_bytes);
-                Ok(())
+                }
+                Ok(Err(err)) => {
+                    println!("Tunnel error: {:?}", err);
+
+                }
+                Err(_) => {
+                    println!("Tunnel timed out");
+                }
             }
-            Ok(Err(err)) => {
-                println!("Tunnel error: {:?}", err);
-                Err(err)
-            }
-            Err(_) => {
-                println!("Data transfer timed out");
-                Err(io::Error::new(io::ErrorKind::TimedOut, "Data transfer timed out"))
-            }
-        }
+        });
+        Ok(Response::new(Body::empty()))
 
     }
+
+
 
 
 
